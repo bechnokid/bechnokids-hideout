1b4dab2351e0d0d733f40af2b2038f3c846b2942
Bechno Kid <bechnokid@gmail.com>
2026-03-01 22:10:15 -0500

    initial commits

--- src/_data/about/ships.mjs
+++ src/_data/about/ships.mjs
@@ -210,14 +210,4 @@ export default {
       }
     ]
   },
-  ocxcanon: [
-    "Stella Aiba (OC) x Alphamon *(Digimon Story Cyber Sleuth)*",
-    "Avis Hunter (♀ OC) x Nadia *(Monster Hunter Wilds)*",
-    "Siduri (OC) x Gilgamesh *(FFVII Remake)*",
-    "Siduri (OC) x Gilgamesh *(Chocobo Tales)*",
-    "Hisui Aoyama (OC) x Haganezuka *(Demon Slayer)*",
-    "Kaybee (OC) x Whisper *(Yokai Watch)*",
-    "Dr. Vitamin (OC) x Professor Membrane *(Invader Zim)*",
-    "Jennifer (OC) & Giovanni *(Pokemon)*"
-  ]
 }
