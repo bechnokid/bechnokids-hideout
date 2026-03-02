83cce7f2522e71f224183a4470e277fbb4d3773a
Bechno Kid <bechnokid@gmail.com>
2026-03-01 22:10:15 -0500

    initial commits

---
+++ src/_data/about/ocxcanon.mjs
@@ -0,0 +1,25 @@
+export default [
+  {
+    oc: {
+      name: "Stella Aiba",
+      img: "",
+      info: "",
+    },
+    canon: {
+      name: "Alphamon",
+      img: "",
+    },
+    ship: {
+      media: "Digimon Story Cyber Sleuth",
+      lore: "",
+    }
+  },
+  "Stella Aiba (OC) x Alphamon *(Digimon Story Cyber Sleuth)*",
+  "Avis Hunter (♀ OC) x Nadia *(Monster Hunter Wilds)*",
+  "Siduri (OC) x Gilgamesh *(FFVII Remake)*",
+  "Siduri (OC) x Gilgamesh *(Chocobo Tales)*",
+  "Hisui Aoyama (OC) x Haganezuka *(Demon Slayer)*",
+  "Kaybee (OC) x Whisper *(Yokai Watch)*",
+  "Dr. Vitamin (OC) x Professor Membrane *(Invader Zim)*",
+  "Jennifer (OC) & Giovanni *(Pokemon)*"
+]
