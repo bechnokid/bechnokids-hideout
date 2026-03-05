baa8b727f3ff6c8cf1652593827600ebbea033b1
Bechno Kid <bechnokid@gmail.com>
2026-03-05 12:25:34 +0000

    added info for cyber sleuth

--- src/_data/about/ocxcanon.mjs
+++ src/_data/about/ocxcanon.mjs
@@ -2,19 +2,43 @@ export default [
   {
     oc: {
       name: "Stella Aiba",
+      age: 27,
       img: "",
-      info: "",
+      info: "Stella Aiba is a human currently working as an assistant in Alphamon's detective agency within the Digital World. She is an otaku with a strong interest in giant mech, often watching anime and building plastic models in her free time. Stella is a little quiet and awkward, but she can be brave when the situation calls for it...unless it involves confessing her feelings towards a certain someone.",
+      trivia: "Stella is an old OC I've had since 2004 before I decided to put her into this story. She has had two redesigns so far: one in 2007 and the most recent one in 2025.",
     },
     canon: {
       name: "Alphamon",
       img: "",
+      age: "???",
+      info: "Alphamon is a Mega-level Digimon and member of the Royal Knights, a group that protects the Digital World under Yggdrasil's orders. She stands out among her fellow knights by possessing a nearly endless amount of knowledge of the both the Digital and the human world. The lone hermit may reveal very little about herself, but she would happily talk for hours about any educational topic. After landing in the human world about 2 years before the present, Alphamon has gained a strange habit of brewing the most disgusting coffee drinks down to monkind.",
     },
-    ship: {
+    lore: {
       media: "Digimon Story Cyber Sleuth",
-      lore: "",
+      content: [
+        "*Warning*: Slightly canon-divergent with a few twists to the original story in *Cyber Sleuth* but remains relatively the same. That being said, *beware of spoilers!*"
+      ],
+      readMore: [
+        `In the Digital World, mindless creatures called "Eaters" began consuming data, threatening to destroy the entire Digital World. Alphamon and several other Royal Knights traveled to the human world after discovering its connection to the monsters. Upon crossing the Digital Gate, Alphamon disguised herself as a woman named Kyoko Kuremi and managed to establish her own detective agency to find a method of eliminating the Eaters and further understanding humans.`,
+
+        `Two years later, Alphamon eventually comes across a human named Stella who recently contracted EDEN Syndrome, a mysterious condition that occurs when users of the EDEN virtual word have their mental data eaten by the Eaters, leaving the victims comatose. Stella has a unique case where she not only manages to survive the Eaters' attack, but gains a half-physical, half-digital form. To avoid Stella attracting unwanted attention, Alphamon (as Kyoko) takes the human in as her new assistant at the agency.`,
+
+        `After several events, Stella, Alphamon, and several friends managed to defeat the Mother Eater and restore peace to both the Digital and the human world. Although Stella awoke from her coma, she decided to stay in the Digital World as an ambassador of sorts to humans and work together with Alphamon as her fully fledged partner.`,
+
+        `The two have their work cut out for them, from having to unravel the ongoing mystery of the Eaters to helping both humans and Digimon from their respective worlds. However, as long as Stella gets to return to the human world occasionally to collect models and, most importantly, spend time with Alphamon, she's happy.`,
+      ],
+    },
+    relationship: {
+      tropes: [
+        "interspecies",
+        "slowburn",
+        "cannot spit it out (for Stella)",
+      ],
+      content: [
+        "Alphamon and Stella's relationship is not unlike the famous Holmes and Watson duo's, with Alphamon as the detective and Stella as the assistant. The two share an unlikely close bond as two people who, despite their quirks, possess the drive to help others with their problems, regardless of whether they're human or Digimon.",
+      ]
     }
   },
-  "Stella Aiba (OC) x Alphamon *(Digimon Story Cyber Sleuth)*",
   "Avis Hunter (♀ OC) x Nadia *(Monster Hunter Wilds)*",
   "Siduri (OC) x Gilgamesh *(FFVII Remake)*",
   "Siduri (OC) x Gilgamesh *(Chocobo Tales)*",
